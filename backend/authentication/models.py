5ded25c95a9850c2d00b4845a5244ad3b291ba93
João Giacomin <160275156+MapleSyrup33@users.noreply.github.com>
2026-06-21 20:40:37 -0600

    [Refactor]: created model to auth and refactored views and serializer

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+
+    email = models.EmailField(unique=True, max_length=255)
+    
+    ROLE_CHOICES = (
+        ('admin', 'Admin/Owner'),
+        ('manager', 'Manager'),
+        ('staff', 'Kitchen Staff'),
+    )
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES, default='manager')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
        model = User
-       fields = ["id", "username", "password",]
-       extra_kwargs= {"password": {"write_only": True}}
+       fields = ["id", "email", "username", "password","role"]
+       extra_kwargs= {"password": {"write_only": True},
+                      "role": {"required": False}
+                      }
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
