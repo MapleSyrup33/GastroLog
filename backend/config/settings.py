0a8cb3779d3143dcb7dc5dfecc7c57906ab9eff0
João Giacomin <160275156+MapleSyrup33@users.noreply.github.com>
2026-06-21 19:49:27 -0600

    [Implementation]: created authentication using serializers

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventorysystem',
+    'authentication',
     'rest_framework',
     'corsheaders',
 ]
@@ -135,3 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWS_CREDENTIALS = True
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from authentication.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("authentication/user/register/", CreateUserView.as_view(), name="register"),
+    path("authentication/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("authentication/token/refresh",TokenRefreshView.as_view(), name="refresh"),
+    path("authentication-auth/", include("rest_framework.urls")),
 ]
