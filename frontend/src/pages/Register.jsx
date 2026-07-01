8b36e172965fb114e945c280d5754d2574a8061a
João Giacomin <160275156+MapleSyrup33@users.noreply.github.com>
2026-07-01 13:27:06 -0600

    [Implementation]: created pages for login and register

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/Header"
+function Register({ onNavigate }) {
+    return(
+        <>
+        <Header onNavigate={onNavigate}/>
+        </>
+    )
+}
+
+export default Register
